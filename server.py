29eccd596b2134ff05f2db09ce44f6a75d89b320
B-Howell <bretthowell10@gmail.com>
2024-05-04 09:31:10 -0400

    initial commit

---
+++ server.py
@@ -0,0 +1,20 @@
+import http.server
+import socketserver
+
+class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Cross-Origin-Opener-Policy', 'same-origin')
+        self.send_header('Cross-Origin-Embedder-Policy', 'require-corp')
+        self.send_header('Cross-Origin-Resource-Policy', 'cross-origin')
+        super().end_headers()
+
+# Set the port
+PORT = 8000
+
+handler_object = MyHttpRequestHandler
+
+my_server = socketserver.TCPServer(("", PORT), handler_object)
+
+# Start the server
+print("Serving at port", PORT)
+my_server.serve_forever()
